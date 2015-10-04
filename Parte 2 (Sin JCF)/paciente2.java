13b7acbe5ddf66ae4b5666643e4edd7c87ecb89c
ochoa95 <och14230@uvg.edu.gt>
2015-10-03 23:51:35 -0600

    Update paciente2.java

--- Parte 2 (Sin JCF)/paciente2.java
+++ Parte 2 (Sin JCF)/paciente2.java
@@ -1,72 +1,94 @@
+
 /**
- * @author Delbert Custodio, 14245; Angel Ochoa, 14320
  *
+ * @author JoseAngel
  * @param <E>
  */
-public class paciente2<E> implements Comparable<E>{
+public class paciente<E> implements Comparable<E>{
 	
-	/**  */
+	/** The nombre paciente. */
 	private String nombre;
 	
-	/**  */
+	/** The enfermedad. */
 	private String caso;
 	
-	/**  */
+	/** The prioridad. */
 	private String prioridad;
 
 	/**
-	 * Constructor
+	 * Instantiates a new paciente.
+	 *
+     * @param Paciente
+	 * @param nombrePaciente the nombre paciente
+     * @param Caso
+     * @param Prioridad
+	 * @param enfermedad the enfermedad
+	 * @param prioridad the prioridad
 	 */
-	public paciente2(String Paciente, String Caso, String Prioridad) {
+	public paciente(String Paciente, String Caso, String Prioridad) {
 		super();
 		this.nombre = Paciente;
 		this.caso = Caso;
 		this.prioridad = Prioridad;
 	}
 
-
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	public int compareTo(E Paciente) {
 		// TODO Auto-generated method stub
-		return this.prioridad.compareTo(((paciente2) Paciente).getPrioridad());
+		return this.prioridad.compareTo(((paciente ) Paciente).getPrioridad());
 	}
 
 	/**
-	 * Setters y Getters de aqui para abajo
+	 * Gets the nombre paciente.
+	 *
+	 * @return the nombre paciente
 	 */
 	public String getNombrePaciente() {
 		return nombre;
 	}
 
 	/**
-	 * 
+	 * Sets the nombre paciente.
+	 *
+	 * @param nombrePaciente the new nombre paciente
 	 */
 	public void setNombrePaciente(String Paciente) {
 		this.nombre = Paciente;
 	}
 
 	/**
-	 * 
+	 * Gets the enfermedad.
+	 *
+	 * @return the enfermedad
 	 */
 	public String getCaso() {
 		return caso;
 	}
 
 	/**
-	 * 
+	 * Sets the enfermedad.
+	 *
+	 * @param enfermedad the new enfermedad
 	 */
 	public void setEnfermedad(String enfermedad) {
 		this.caso = enfermedad;
 	}
 
 	/**
-	 * 
+	 * Gets the prioridad.
+	 *
+	 * @return the prioridad
 	 */
 	public String getPrioridad() {
 		return prioridad;
 	}
 
 	/**
-	 * 
+	 * Sets the prioridad.
+	 *
+	 * @param prioridad the new prioridad
 	 */
 	public void setPrioridad(String prioridad) {
 		this.prioridad = prioridad;
