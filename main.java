a97dfdd7126b48bf8b20562d09fafb55a4422e74
DelbertCV <dfcustodiovargas@gmail.com>
2015-10-03 20:40:41 -0600

    Se imprime el lista de pacientes
    Problema: no funciona todavia la PriorityQueue

--- main.java
+++ main.java
@@ -11,20 +11,28 @@ public class main {
 	
 	public static void main(String[] args){
 		
-		Queue<paciente> V = new PriorityQueue();
-		int nopa = 0;
+		
 		
 		try{
 			
 			FileInputStream Stream = new FileInputStream("pacientes.txt");
 			DataInputStream Stream2 = new DataInputStream(Stream);
 			BufferedReader Stream3 = new BufferedReader(new InputStreamReader(Stream2));
+			VectorHeap<String> V = new VectorHeap<String>();
 			String Linea;
+			int nopa = 0;
 			
 			int i=0;
 			int principio =0;
 			int nop =0;
 			String[] partes = new String[3];
+			
+			
+			System.out.println("Listado de Pacientes: \n");
+			while ((Linea = Stream3.readLine())!=null){
+				System.out.println(Linea+"\n");
+			}
+			
 			
 			while ((Linea = Stream3.readLine())!=null){
 				
@@ -32,27 +40,20 @@ public class main {
 				nop = 0;
 				i=0;
 				
-				for(int n = 0; n <  Linea.length(); n++){
-					if (Character.toString(Linea.charAt(n)).equals(",") && i <3)
-					{
-						partes[i] = Linea.substring(principio,n);
-						principio = n+1;
-						nop = nop +1;
-						i = i+1;	
-					}	
-				}
-				i =0;
-				partes[2] = Linea.substring(principio+1,Linea.length());
-				V.add(new paciente<String>(partes[0],partes[1],partes[2]));
+				V.add(Linea);
 				nopa = nopa +1;
+
 			}
 			Stream2.close();
 			
-			 paciente<String>  ficha;  
-				for(int i1=0; i1<nopa; i1++){
-					ficha = V.poll();
-					System.out.println(ficha.getNombre()+","+ficha.getCaso()+", "+ficha.getPrioridad());
-				}	
+			String lista;
+			for (i=0;i<nopa;i++){
+				lista = V.remove();
+				System.out.println(lista);
+			}
+			
+			
+			
 		}
 		catch (Exception e){
 			System.out.println(e);
